d68ed3a1ab7f4aabe3f8e670b882f994612f8e94
unknown <vinigusto07@gmail.com>
2019-02-06 01:40:03 -0200

    code

---
+++ RPS.py
@@ -0,0 +1,30 @@
+# PEDRA PAPEL TESOURA
+#  PARA RODAR NO CMD python RPS.py
+
+from random import randint
+
+options = ["ROCK", "PAPER", "SCISSORS"]
+
+message = {
+  "tie": "Yawn it's a tie!",
+  "won": "Yay you won!",
+  "lost": "Aww you lost!"
+}
+
+def decide_winner(user_choice, computer_choice):
+  print("You selected: %s" % user_choice)
+  print("The computer selected: %s" % computer_choice)
+  if computer_choice == user_choice:
+    print(message["tie"])
+  elif (user_choice == options[0] and computer_choice == options[2]) or (user_choice == options[1] and computer_choice == options[0]) or (user_choice == options[2] and computer_choice == options[1]):
+    print(message["won"])
+  else:
+    print(message["lost"])
+    
+def play_RPS():
+  user_choice = input("Enter Rock, Paper, or Scissors: ")
+  user_choice = user_choice.upper()
+  computer_choice = options[randint(0,2)]
+  decide_winner(user_choice, computer_choice)
+  
+play_RPS()
